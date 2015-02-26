6c41027343dbc7c4674c20446cd3e9417ccf0405
Ben Pritchard and Kameron Johnson <Ben.S.Pritchard@gmail.com>
2015-02-26 10:47:00 -0800

    Add styling to posts and upvote glypyicon.

--- services/ArticlesFactory.js
+++ services/ArticlesFactory.js
@@ -3,10 +3,16 @@ whackerNews.factory('ArticlesFactory', function ArticlesFactory() {
   factory.articles = [];
   factory.addArticle = function() {
     var postedDate = new Date();
-    factory.articles.push({ name: factory.articleName, id: factory.articles.length + 1,
-    link: factory.linkName, votes: 0, datePosted: postedDate, comments: []});
-    factory.articleName = null;
-    factory.linkName = null;
+    factory.articles.push({ name: factory.articleName,
+      id: factory.articles.length + 1,
+      link: factory.linkName, votes: 0,
+      datePosted: postedDate, comments: []});
+        factory.articleName = null;
+        factory.linkName = null;
+
+  };
+  factory.upVote = function(article) {
+    article.votes += 1;
   };
   return factory;
 });
